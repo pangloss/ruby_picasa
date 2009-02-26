205696b7370bce329b2acc89837f4499ac9b22fd
Darrick Wiebe <darrick@innatesoftware.com>
2009-02-25 20:02:49 -0500

    Set up a shared description for the Base class

--- spec/ruby_picasa/types_spec.rb
+++ spec/ruby_picasa/types_spec.rb
@@ -3,97 +3,70 @@ require 'pp'
 
 include RubyPicasa
 
-describe Base do
-  describe 'for Album' do
-    before :all do
-      @xml = open_file('album.atom').read
-    end
+describe 'a RubyPicasa document', :shared => true do
+  it 'should have an id' do
+    @object.id.should_not be_nil
+  end
 
-    before do
-      @parent = mock('parent')
-      @album = Album.new(@xml, @parent)
-      @album.session = mock('session')
-    end
+  it 'should have an author' do
+    @object.author.should_not be_nil
+    @object.author.name.should == 'Liz'
+    @object.author.uri.should == 'http://picasaweb.google.com/liz'
+  end
 
-    it 'should have an id' do
-      @album.id.should_not be_nil
-    end
+  it 'should get links by name' do
+    @object.link('abc').should be_nil
+    @object.link('alternate').href.should_not be_nil
+  end
 
-    it 'should have an author' do
-      @album.author.should_not be_nil
-      @album.author.name.should == 'Liz'
-      @album.author.uri.should == 'http://picasaweb.google.com/liz'
-    end
+  it 'should do nothing for previous and next' do
+    @object.previous.should be_nil
+    @object.next.should be_nil
+  end
 
-    it 'should get links by name' do
-      @album.link('abc').should be_nil
-      @album.link('alternate').href.should == 'http://picasaweb.google.com/liz/Lolcats'
-    end
+  it 'should load' do
+    @object.session.expects(:get_url).with(@object.id, {})
+    @object.load
+  end
 
-    it 'should do nothing for previous and next' do
-      @album.previous.should be_nil
-      @album.next.should be_nil
-    end
-
-    it 'should load' do
-      @album.session.expects(:get_url).with(@album.id, {})
-      @album.load
-    end
-
-    it 'should have 6 links' do
-      @album.links.length.should == 6
-      @album.links.each do |l|
-        l.should be_an_instance_of(Objectify::Atom::Link)
-      end
-    end
-
-    describe 'session' do
-      it 'should return @session' do
-        @album.session = :sess
-        @album.session.should == :sess
-      end
-
-      it 'should get the parent session' do
-        @album.session = nil
-        @parent.expects(:session).returns(:parent_sess)
-        @album.session.should == :parent_sess
-      end
-
-      it 'should be nil if no parent' do
-        @album.session = nil
-        @album.expects(:parent).returns nil
-        @album.session.should be_nil
-      end
-    end
-
-    describe 'first Photo' do
-      before do
-        @photo = @album.entries.first
-        @photo.should be_an_instance_of(Photo)
-      end
-
-      it 'should have a content' do
-        @photo.content.should be_an_instance_of(PhotoUrl)
-      end
-
-      it 'should have 3 thumbnails' do
-        @photo.thumbnails.length.should == 3
-        @photo.thumbnails.each do |t|
-          t.should be_an_instance_of(PhotoUrl)
-        end
-      end
+  it 'should have links' do
+    @object.links.should_not be_empty
+    @object.links.each do |l|
+      l.should be_an_instance_of(Objectify::Atom::Link)
     end
   end
 
+  describe 'session' do
+    it 'should return @session' do
+      @object.session = :sess
+      @object.session.should == :sess
+    end
+
+    it 'should get the parent session' do
+      @object.session = nil
+      @parent.expects(:session).returns(:parent_sess)
+      @object.session.should == :parent_sess
+    end
+
+    it 'should be nil if no parent' do
+      @object.session = nil
+      @object.expects(:parent).returns nil
+      @object.session.should be_nil
+    end
+  end
+end
+
+
 describe User do
+  it_should_behave_like 'a RubyPicasa document'
+
   before :all do
     @xml = open_file('user.atom').read
-    @class = User
   end
 
   before do
     @parent = mock('parent')
-    @user = User.new(@xml, @parent)
+    @object = @user = User.new(@xml, @parent)
     @user.session = mock('session')
   end
 
@@ -107,18 +80,25 @@ describe RecentPhotos do
 end
 
 describe Album do
+  it_should_behave_like 'a RubyPicasa document'
+
   before :all do
     @xml = open_file('album.atom').read
   end
 
   before do
     @parent = mock('parent')
-    @album = Album.new(@xml, @parent)
+    @object = @album = Album.new(@xml, @parent)
     @album.session = mock('session')
   end
 
   it 'should have 1 entry' do
     @album.entries.length.should == 1
+  end
+
+  it 'should get links by name' do
+    @album.link('abc').should be_nil
+    @album.link('alternate').href.should == 'http://picasaweb.google.com/liz/Lolcats'
   end
 
   describe 'photos' do
@@ -156,13 +136,23 @@ describe Album do
     it 'should not have an author' do
       @photo.author.should be_nil
     end
+
+    it 'should have a content' do
+      @photo.content.should be_an_instance_of(PhotoUrl)
+    end
+
+    it 'should have 3 thumbnails' do
+      @photo.thumbnails.length.should == 3
+      @photo.thumbnails.each do |t|
+        t.should be_an_instance_of(PhotoUrl)
+      end
+    end
   end
 
 end
 
-  describe 'for Search' do
-    it 'should request next'
-    it 'should request previous'
-  end
-
+describe Search do
+  it 'should request next'
+  it 'should request previous'
 end
+
